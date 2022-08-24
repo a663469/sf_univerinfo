8eebe75fcee0f6178247d9f7e1e3da1b7b749339
Sabr <a663469@gmail.com>
2022-08-24 09:30:22 +0300

    Add gitignore, modified CSVReader

--- src/main/java/io/CSVReader.java
+++ src/main/java/io/CSVReader.java
@@ -6,12 +6,15 @@ import java.util.Scanner;
 
 public class CSVReader {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("in/cap.csv"));
+        Scanner scanner = new Scanner(new FileInputStream("data/students.csv"));
         String[] headers = scanner.nextLine().split(";");
 //        System.out.format(leftAlignFormat, headers);
         while (scanner.hasNextLine()) {
             String[] position = scanner.nextLine().split(";", -1);
-
+            for(String word : position) {
+                System.out.printf(word + " ");
+            }
+            System.out.println();
         }
     }
 }
