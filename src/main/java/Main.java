c0ba7e454d3bc00e7d14e29e3d704b81534ccf8b
Sabr <a663469@gmail.com>
2022-11-27 19:49:28 +0300

    В соответствии требовании HW28, убрано команды свзанные с JSON

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,41 +27,13 @@ public class Main {
         StudentComparator studentComparator = ComparatorUtil.getStudentComparator(StudentComparatorVariants.NAME);
 //        students.stream().sorted(studentComparator).forEach(System.out::println);
 
-
-        // Конвертируем объекты "студенты" в json формат
-        String studentsJson = studentListToJson(students);
-        // С json формат создаем объекты "студенты"
-        List<Student> studentsFromJson = jsonToStudentList(studentsJson);
-        // Сровнение количество объектов
-//        System.out.printf("Количество до и после преобрзазование совпадает? ");
-//        System.out.println(students.size() == studentsFromJson.size() ? "ДА!" : "НЕТ!");
-
-        // С помощью Java Stream API выполнить для исходных коллекций сериализацию отдельных элементов.
-        students.forEach(student -> {
-        // Там же внутри стрима выводить получающиеся JSON-строки.
-            String studentJson = studentToJson(student);
-            // Там же внутри стрима десериализовывать объекты из полученных JSON-строк.
-            // Там же внутри стрима выводить десериализованные объекты на печать, чтобы убедиться в корректности операции.
-            Student studentFromJson = jsonToStudent(studentJson);
-        });
-
         List<University> universities = UniversitiesFromFile.getUniversities("data/universityInfo.xlsx", "Университеты");
-
 //        universities.stream().forEach(System.out::println);
 
         UniversityComparator universityComparator = ComparatorUtil.getUniversityComparator(UniversityComparatorVariants.YEAR_OF_FOUNDATION);
 //        universities.stream().sorted(universityComparator).forEach(System.out::println);
 
-        String universitiesJson = universityListToJson(universities);
-        List<University> universitiesFromJson = jsonToUniversityList(universitiesJson);
-        universities.forEach(university -> {
-            String universityJson = universityToJson(university);
-            University universityFromJson = jsonToUniversity(universityJson);
-        });
-
-        new XlsWriter(StatisticsByProfile.getStatisticsList(students, universities), "temp.xls");
         new XlsWriter(StatisticsByProfile.createStatistics(students, universities), "temp_stream.xls");
-
 
         log.info("Программа заверщена!");
     }
